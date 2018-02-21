cf273b7907e0751d1ff0379c106277f9ed110456
Novikov Andrey <a.novikov@pr1me.ru>
2018-02-21 18:40:16 +0300

    Change VK API version

--- Provider.php
+++ Provider.php
@@ -47,7 +47,7 @@ class Provider extends AbstractProvider implements ProviderInterface
         $lang = $this->getConfig('lang');
         $lang = $lang ? '&language=' . $lang : '';
         $version = $this->getConfig('version');
-        $version = $version ? '&v=5.8' : '';
+        $version = $version ? '&v=5.73' : '';
         $response = $this->getHttpClient()->get(
             'https://api.vk.com/method/users.get?access_token=' . $token . '&fields=' . implode(',', $this->fields) . $lang . $version
         );
